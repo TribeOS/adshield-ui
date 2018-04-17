b06b2013cb63910abc4642c3146ffc0220785eff
amanadriano <amanadriano@yahoo.com>
2018-04-17 09:11:24 +0800

    refresh fiels

--- dist/assets/adshield-front.js
+++ dist/assets/adshield-front.js
@@ -1243,6 +1243,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("adshield-front/app")["default"].create({"LOG_RESOLVER":true,"LOG_ACTIVE_GENERATION":true,"LOG_TRANSITIONS":true,"LOG_TRANSITIONS_INTERNAL":true,"LOG_VIEW_LOOKUPS":true,"name":"adshield-front","version":"0.0.0+eabde5d2"});
+  require("adshield-front/app")["default"].create({"LOG_RESOLVER":true,"LOG_ACTIVE_GENERATION":true,"LOG_TRANSITIONS":true,"LOG_TRANSITIONS_INTERNAL":true,"LOG_VIEW_LOOKUPS":true,"name":"adshield-front","version":"0.0.0+c87cad2f"});
 }
 //# sourceMappingURL=adshield-front.map
