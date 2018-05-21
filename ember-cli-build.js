c8035a21fe427e2c2bf54ba2116df6c15ce77b66
joey <joey@tr.be>
2018-05-21 13:08:35 +0800

    added glyphicon font

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,6 +20,7 @@ module.exports = function(defaults) {
   }
   });
 	app.import('bower_components/bootstrap/dist/css/bootstrap.min.css')
+	app.import('bower_components/bootstrap/dist/js/bootstrap.js')
 
 
   // Use `app.import` to add additional libraries to the generated
