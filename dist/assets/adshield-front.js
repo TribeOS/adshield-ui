b28d45ab438654d0a9d132b82c7b1604c46b1c9b
joey <joey@tr.be>
2018-05-23 10:59:24 +0800

    edit line 235 on vendor css

--- dist/assets/adshield-front.js
+++ dist/assets/adshield-front.js
@@ -2495,6 +2495,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("adshield-front/app")["default"].create({"LOG_RESOLVER":true,"LOG_ACTIVE_GENERATION":true,"LOG_TRANSITIONS":true,"LOG_TRANSITIONS_INTERNAL":true,"LOG_VIEW_LOOKUPS":true,"name":"adshield-front","version":"0.0.0+2c03913b"});
+  require("adshield-front/app")["default"].create({"LOG_RESOLVER":true,"LOG_ACTIVE_GENERATION":true,"LOG_TRANSITIONS":true,"LOG_TRANSITIONS_INTERNAL":true,"LOG_VIEW_LOOKUPS":true,"name":"adshield-front","version":"0.0.0+973fc6b3"});
 }
 //# sourceMappingURL=adshield-front.map
