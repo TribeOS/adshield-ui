105dd9349d5e1bf28cdaff6bb04422f305bccad4
aman <aman@tr.be>
2018-05-23 09:38:15 +0800

    pulled from

---
+++ tests/unit/controllers/dash/ipaccesslist-test.js
@@ -0,0 +1,12 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Controller | dash/ipaccesslist', function(hooks) {
+  setupTest(hooks);
+
+  // Replace this with your real tests.
+  test('it exists', function(assert) {
+    let controller = this.owner.lookup('controller:dash/ipaccesslist');
+    assert.ok(controller);
+  });
+});
