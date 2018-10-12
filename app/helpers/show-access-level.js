0835f8d2bd3751b5b72531708f3ef0fbd00f75be
amanadriano <amanadriano@yahoo.com>
2018-10-12 09:38:42 +0800

    fix bug on access level

--- app/helpers/show-access-level.js
+++ app/helpers/show-access-level.js
@@ -1,8 +1,7 @@
 import { helper } from '@ember/component/helper';
 
 export function showAccessLevel(params/*, hash*/) {
-	console.log(params[0]);
-	switch(params[0]) {
+	switch(parseInt(params[0])) {
 		case 1 : return "Account Access"; break;
 		case 2 : return "Report Access"; break;
 		case 3 : return "Settings Access"; break;
