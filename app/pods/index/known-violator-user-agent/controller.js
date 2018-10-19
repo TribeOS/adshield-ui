fef62c490d027f4d0813f10ddf9a5d8f07ca1e45
aman <secondrnd@gmail.com>
2018-10-19 16:34:22 +0800

    test view for knonw violator user agent

--- app/pods/index/known-violator-user-agent/controller.js
+++ app/pods/index/known-violator-user-agent/controller.js
@@ -12,4 +12,20 @@ export default IpBaseController.extend({
 		this.sort = { by : "last_updated", dir : "asc" };
 	},
 
+	actions : {
+		refresh() {
+			let self = this;
+            this.set("page", 1);
+            this.fetchMySites(function(data) {
+            	self.filter.userKey = self.userWebsites.objectAt(0).get("userKey");
+            	self.refreshList(self.page, self.limit, self.filter, self.sort);
+            });
+        },
+        onSelectSite(item) {
+        	let self = this;
+        	self.filter.userKey = item;
+        	self.refreshList(self.page, self.limit, self.filter, self.sort);
+        }
+	}
+
 });
