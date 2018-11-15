b99558a89646e431c8f5d440e4e43138043c0826
aman <secondrnd@gmail.com>
2018-11-15 14:48:24 +0800

    updated frontend for protection pages

--- app/pods/index/blocked-country/controller.js
+++ app/pods/index/blocked-country/controller.js
@@ -12,4 +12,23 @@ export default IpBaseController.extend({
 		this.sort = { by : "last_updated", dir : "asc" };
 	},
 
+
+	actions : {
+		refresh() {
+			let self = this;
+            this.set("page", 1);
+            this.fetchMySites(function(data) {
+            	self.filter.userKey = self.userWebsites.objectAt(0).get("userKey");
+            	self.refreshList(self.page, self.limit, self.filter, self.sort);
+            });
+        },
+        onSelectSite(item) {
+        	let self = this;
+        	this.set("page", 1);
+        	self.filter.userKey = item;
+        	self.refreshList(self.page, self.limit, self.filter, self.sort);
+        }
+	}
+	
+
 });
