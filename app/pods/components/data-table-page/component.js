11f2737cdfdd054338f4ad548910a1c2eed061a7
aman <aman@tr.be>
2018-11-16 14:20:51 +0800

    updated api list

--- app/pods/components/data-table-page/component.js
+++ app/pods/components/data-table-page/component.js
@@ -1,4 +1,20 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+
+	actions : {
+		firstPage() {
+			this.sendAction("firstPage");
+		},
+		previousPage() {
+			this.sendAction("previousPage");
+		},
+		nextPage() {
+			this.sendAction("nextPage");
+		},
+		lastPage() {
+			this.sendAction("lastPage");
+		}
+	}
+
 });
