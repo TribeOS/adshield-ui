70e825d1f8f89ae7db61792e62af00c9b82d18b7
aman <secondrnd@gmail.com>
2019-03-13 14:46:50 +0800

    updated pagination

--- app/pods/components/data-table-page/component.js
+++ app/pods/components/data-table-page/component.js
@@ -1,7 +1,50 @@
 import Component from '@ember/component';
+import { computed } from "@ember/object";
 
 
 export default Component.extend({
+
+	pages : [],
+
+
+	didUpdateAttrs() {
+ 	   	this._super(...arguments);
+ 
+		let pg = [];
+		if (this.source.total == 0) return;
+
+		if (this.source.last_page <= 5) {
+			for(var i = 1; i <= this.source.last_page; i++) {
+				pg.push(i);
+			}
+			this.set("pages", pg);
+			return;
+		}
+
+		pg.push(1);
+		//get half
+		//get 2 pages left
+		//get 2 pages right
+		// let mid = Math.floor(this.source.last_page / 2);
+		let mid = this.source.current_page;
+		if (mid  - 3 > 1) pg.push("..");
+		for(var i = 3; i > 0; i --) {
+			if (mid - i <= pg[0]) continue;
+			pg.push(mid - i);
+		}
+		if (mid > 1 && mid < this.source.last_page) pg.push(mid);
+
+		for(var i = 1; i < 4; i++) {
+			if (mid + i >= this.source.last_page) continue;
+			pg.push(mid + i);
+		}
+		if (mid  + 3 < this.source.last_page) pg.push("..");
+		pg.push(this.source.last_page);
+
+		this.set("pages", pg);
+
+	},
+
 
 	actions : {
 		firstPage() {
