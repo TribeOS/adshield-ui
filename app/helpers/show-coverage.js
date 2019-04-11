f4b75339dd49ae773411eabc26e61d46bff777b8
aman <aman@tr.be>
2019-04-11 14:05:46 +0800

    update email notification choices

--- app/helpers/show-coverage.js
+++ app/helpers/show-coverage.js
@@ -3,8 +3,8 @@ import { helper } from '@ember/component/helper';
 export function showCoverage(params/*, hash*/) {
 	switch(params[0]) {
 		case "all" : return "All Events"; break;
-		case "settings" : return "Settings"; break;
-		case "violations" : return "Violations"; break;
+		case "settings" : return "Settings Updated"; break;
+		case "violations" : return "Violations Detected"; break;
 	}
 }
 
